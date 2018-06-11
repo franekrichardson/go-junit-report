74e4e7f35849727bf77a9aa91662901eae8e07b7
franek <franek@elastic.co>
2018-06-11 23:02:06 +0200

    Add file output and stdout printing
    Adds an explicit file output option, when specified it will
    write the test report to the given file, and stdout will
    print the original `go test` output.
    
    If verbose is specified the full `go test -v` output will
    be printed to screen.

--- go-junit-report.go
+++ go-junit-report.go
@@ -14,6 +14,8 @@ var (
 	packageName   string
 	goVersionFlag string
 	setExitCode   bool
+	outputFile    string
+	verbose       bool
 )
 
 func init() {
@@ -21,20 +23,44 @@ func init() {
 	flag.StringVar(&packageName, "package-name", "", "specify a package name (compiled test have no package name in output)")
 	flag.StringVar(&goVersionFlag, "go-version", "", "specify the value to use for the go.version property in the generated XML")
 	flag.BoolVar(&setExitCode, "set-exit-code", false, "set exit code to 1 if tests failed")
+	flag.StringVar(&outputFile, "output-file", "", "specify the output file to print the test report to, if specified then stdout will print the original output")
+	flag.BoolVar(&verbose, "verbose", false, "print verbose test output, equivalent to test -v")
 }
 
 func main() {
 	flag.Parse()
 
+	outputLevel :=  parser.NoOutput
+	if(outputFile != "") {
+		if(verbose) {
+			outputLevel = parser.FullOutput
+		} else {
+			outputLevel = parser.BasicOutput
+		}
+	}
+
 	// Read input
-	report, err := parser.Parse(os.Stdin, packageName)
+	report, err := parser.Parse(os.Stdin, packageName, outputLevel)
 	if err != nil {
 		fmt.Printf("Error reading input: %s\n", err)
 		os.Exit(1)
 	}
 
+	writer := os.Stdout
+
+	if(outputFile != "") {
+		f, err := os.Create(outputFile)
+		if err != nil {
+			fmt.Printf("Error opening output file: %s\n", err)
+			os.Exit(1)
+		}
+		writer = f
+	
+		defer f.Close()
+	}
+
 	// Write xml
-	err = formatter.JUnitReportXML(report, noXMLHeader, goVersionFlag, os.Stdout)
+	err = formatter.JUnitReportXML(report, noXMLHeader, goVersionFlag, writer)
 	if err != nil {
 		fmt.Printf("Error writing XML: %s\n", err)
 		os.Exit(1)
